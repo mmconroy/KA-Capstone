11e2ff1c8527da0bcd90a8e2f2dea726bd0fbd06
benfindley18 <ben.kenzieacademy@gmail.com>
2020-07-02 12:49:25 -0400

    styled savings modal

--- src/Goals/GoalModal.js
+++ src/Goals/GoalModal.js
@@ -1,5 +1,6 @@
 import React, { useState, Component } from 'react';
 import Modal from 'react-modal'
+import './GoalModal.scss'
 import {
     withRouter,
 } from "react-router-dom";
@@ -10,31 +11,53 @@ function NewGoal() {
     const [modalIsOpen, setModalIsOpen] = useState(false)
     return (
         <>
-            <img onClick={() => setModalIsOpen(true)} src="https://img.icons8.com/flat_round/64/000000/plus.png" />
+            <div className='header'>
+                <h1> Your Savings </h1>
+                <img onClick={() => setModalIsOpen(true)} src="https://img.icons8.com/ios/50/000000/add.png" />
+            </div>
 
-            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-                <form className='goalForm'>
+            <div className='emptyStaticScreen'>
+                <p>You have not created any savings goals yet, click the plus button to get started on your first goal!</p>
+                <img src="https://img.icons8.com/ios/100/000000/sad-cloud.png" />
+            </div>
 
-                    <input type='text' name='goalName' className='goalName' placeholder=' Set Goal Name ' />
+            <div className='footer'>
 
-                    <label htmlFor='password'> Goal Amount: </label> <br />
-                    <input type='number' name='goalAmount' className='goalAmount' min='0.00' step='0.01' />
+            </div>
 
-                    <label htmlFor='depositFrequency'> Deposit Frequency: </label> <br />
-                    <select className='depositFrequency' name='depositFrequency' form='goalForm'>
-                        <option value='weekly'> Weekly </option>
-                        <option value='bi-weekly'> Bi-Weekly </option>
-                        <option value='monthly'> Monthly </option>
-                    </select>
+            <Modal className='modal' isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                <div className='formDiv'>
+                    <form className='goalForm'>
+                        <div className='newGoalName'>
+                            <label className='label' htmlFor='goalName'> Goal Name: </label> <br />
+                            <input type='text' name='goalName' className='goalName input' />
+                        </div>
 
-                    <label htmlFor='depositAmount'> Deposit Amount: </label> <br />
-                    <input type='number' name='goalAmount' className='depositAmount' min='0.00' step='0.01' />
+                        <div className='newGoalAmount'>
+                            <label className='label' htmlFor='goalAmount'> Goal Amount: </label> <br />
+                            <input type='number' name='goalAmount' className='goalAmount input' min='0.00' step='0.01' />
+                        </div>
 
-                    <input type="submit" value="Save Goal" className='saveGoal' />
-                </form>
+                        <div className='NewDepositFrequency'>
+                            <label className='label' htmlFor='depositFrequency'> Deposit Frequency: </label> <br />
+                            <select className='depositFrequency input' name='depositFrequency' form='goalForm'>
+                                <option value='weekly'> Weekly </option>
+                                <option value='bi-weekly'> Bi-Weekly </option>
+                                <option value='monthly'> Monthly </option>
+                            </select>
+                        </div>
+
+                        <div className='NewDepositAmount'>
+                            <label className='label' htmlFor='depositAmount'> Deposit Amount: </label> <br />
+                            <input type='number' name='goalAmount' className='depositAmount input' min='0.00' step='0.01' />
+                        </div>
+
+                        <input type="submit" value="Save Goal" className='saveGoal' />
+                    </form>
+                </div>
             </Modal>
         </>
     )
 }
 
-export default withRouter(NewGoal)
+export default NewGoal
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import NewGoal from './Goals/GoalModal'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NewGoal />
   </React.StrictMode>,
   document.getElementById('root')
 );
