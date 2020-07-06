7d40782ad995863d04d05573630608c98dd59275
benfindley18 <ben.kenzieacademy@gmail.com>
2020-07-06 12:29:09 -0400

    created goal details page

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Details from './Goals/GoalsDetail'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Details />
   </React.StrictMode>,
   document.getElementById('root')
 );
