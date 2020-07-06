de824660d5505e419fbf9ab8fc676e23f26a8703
Matthew Conroy <mmconroy@gmail.com>
2020-07-05 22:27:08 -0400

    Bottom Nav completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Landing from "./Landing/Landing";
 import TopMenu from "./Menu/TopMenu";
+import BottomMenu from "./Menu/BottomMenu";
 
 function App() {
   return (
     <div className="App">
       <TopMenu />
+      <BottomMenu />
     </div>
   );
 }
