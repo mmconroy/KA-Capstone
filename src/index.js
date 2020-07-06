f99e0c055ea08d6ba33ba8a97a7e2e71dbc4abfd
benfindley18 <ben.kenzieacademy@gmail.com>
2020-07-06 10:18:53 -0400

    Added goal modal styling

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import NewGoal from './Goals/GoalModal'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <NewGoal />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
