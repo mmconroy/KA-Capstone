0c0304e6865b9f53355b4d03cff0c9d729cef9c0
benfindley18 <ben.kenzieacademy@gmail.com>
2020-07-09 11:11:30 -0400

    Restyled goal/input screen

--- src/Goals/GoalsDetail.js
+++ src/Goals/GoalsDetail.js
@@ -8,30 +8,9 @@ class Details extends Component {
   render() {
     return (
       <>
-        {/* <div className='header'>
-                    <h1> Your Savings </h1>
-                </div> */}
-
-        <div className="goal">
-          <div className="hexagon">
-            <svg
-              width="61"
-              height="70"
-              viewBox="0 0 81 90"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M40.216 9.288L71.143 27.144V62.856L40.216 80.712L9.289 62.856V27.144L40.216 9.288ZM40.216 0C38.612 0 37.01 0.415 35.571 1.244L4.645 19.1C1.773 20.761 0 23.826 0 27.144V62.856C0 66.175 1.772 69.24 4.645 70.9L35.572 88.756C37.01 89.585 38.613 90 40.217 90C41.821 90 43.423 89.585 44.862 88.756L75.789 70.9C78.661 69.239 80.434 66.174 80.434 62.856V27.144C80.434 23.825 78.662 20.76 75.789 19.1L44.861 1.244C43.422 0.415 41.82 0 40.216 0Z"
-                fill="black"
-              />
-            </svg>
-          </div>
-
-          <div className="progress">
-            <h3 className="goalName"> Goal Name </h3>
-            <Progress percent={0} />
-          </div>
+        <div className="progressBar">
+          <h3 className="goalName"> Goals </h3>
+          <Progress percent={0} />
         </div>
 
         <div className="goalFooter">
@@ -39,21 +18,24 @@ class Details extends Component {
           <hr />
         </div>
 
-        <div className="recentActivity">
-          <h2>
-            {" "}
-            No recent activity. Once money has been moved into savings, it will
-            show up here.
-          </h2>
+        <div className="emptyScreen">
           <div className="img">
             <img
               className="bankIcon"
               src="https://img.icons8.com/dusk/64/000000/money-box.png"
             />
           </div>
+          <h2>
+            Once you create your first goal it will appear here.
+            Press the button below to add a new goal.
+          </h2>
         </div>
 
-        <div className="footer"></div>
+        <div className="newGoal">
+          <button className="addGoal">
+            <span> Add Goal </span>
+          </button>
+        </div>
       </>
     );
   }
